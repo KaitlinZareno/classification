29a44e8d0c25e4f544b554d97e28fb23fc9692b3
KaitlinZareno <kaitlinzareno@gmail.com>
2019-01-06 10:15:36 -0800

    updates to the massClassifier code: it now prints image names and their classification values to the file classList. Prints image names again at the end of classList file

--- dogsEx/massClassifierCode.py
+++ dogsEx/massClassifierCode.py
@@ -8,31 +8,29 @@ import sys
 
 f = open('classList.txt', 'w')  
 
-#path = '/home/kzareno/Desktop/sandbox/research/RatVideos/*.jpg'
-path = '/home/kzareno/example_code/dogsEx/*.jpg'  
+path = '/home/kzareno/classification/dogsEx/*.jpg'  
 count =0
-#chmod +x label_image.py
+
 x1 =  'label_image.py' 
 x2 = '--graph=./retrained1_graph.pb'
 x3 = '--labels=./retrained1_labels.txt'
 x4 = '--input_layer=Placeholder'
 x5 = '--output_layer=final_result'
-x7 = '>classList.txt'
-#x7 = '| tee classList.txt;'
 
 files=glob.glob(path)   
 for file in files:   
     name  = os.path.basename(file)  
     f.write(name +"\n")
-    x6 = '--image=./'+ name  
+    x6 = '--image=./'+ name 
+    args1 = ['echo', '\n', name]
     argument = ['python', x1, x2, x3, x4, x5,x6]
-    subprocess.call(argument)
-    #args2 = [x7]
-    #subprocess.call(args2)
-    sys.stdout = f
-    f.write("\n\n")
-    #print (filename)
+    subprocess.call(args1, stdout=f)
+    subprocess.call(argument, stdout=f)
+
     count +=1 
 
-#f.close()
+args3 = ['tail', '-n +'+str(count), 'classList.txt']
+#subprocess.call(args3)
+
+#currently prints file names at the end of the classification	
     
